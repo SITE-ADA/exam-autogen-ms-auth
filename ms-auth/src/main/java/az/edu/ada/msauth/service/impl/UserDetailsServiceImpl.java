99347f21faabe75afe9196f410c5d0e665115e90
1javid <jav.alakbarli@gmail.com>
2024-01-22 17:18:10 +0400

    changes for eureka server

--- ms-auth/src/main/java/az/edu/ada/msauth/service/impl/UserDetailsServiceImpl.java
+++ ms-auth/src/main/java/az/edu/ada/msauth/service/impl/UserDetailsServiceImpl.java
@@ -1,11 +1,9 @@
 package az.edu.ada.msauth.service.impl;
 
 
-import az.edu.ada.msauth.model.entities.Contact;
 import az.edu.ada.msauth.model.entities.User;
-import az.edu.ada.msauth.repository.UserDetailsRepository;
 import az.edu.ada.msauth.repository.UserRepository;
-import az.edu.ada.msauth.service.impl.UserDetailsImpl;
+import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,10 +11,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
     UserRepository userRepository;
 
     @Override
